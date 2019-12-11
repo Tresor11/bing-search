6df149f1ffea82f962596a49eea47be03b91b96d
Tresor11 <tresor.moise2001@gmail.com>
2019-12-11 14:35:22 +0200

    added the rest-client logic

--- main.rb
+++ main.rb
@@ -1,7 +1,19 @@
+# frozen_string_literal: true
+
 require 'rest-client'
 class Bing
   def search
-    
+    puts "please enter a search term:"
+    search = gets.chomp
+    url = 'https://www.bing.com/search?'
+    response = RestClient.get(url, { params: { :q => search } })
+    if response.code == 200
+      puts response.headers
+    else
+      puts :error
+    end
   end
-  
 end
+
+try = Bing.new
+try.search
